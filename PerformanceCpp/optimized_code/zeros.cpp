fd53574a5a3511726c88e2f0c8aeeb331e30a8af
Thanh Doan <doant@outlook.com>
2019-06-23 08:50:54 -0700

    Reserve space in memory for vector to improve perf.

--- PerformanceCpp/optimized_code/zeros.cpp
+++ PerformanceCpp/optimized_code/zeros.cpp
@@ -2,21 +2,11 @@
 
 using namespace std;
 
-vector < vector <float> > zeros(int height, int width) {
-	int i, j;
-  
+vector < vector <float> > zeros(int height, int width) { 
 	// OPTIMIZATION: Reserve space in memory for vectors
-	vector < vector <float> > newGrid;
-	vector <float> newRow;
-
   	// OPTIMIZATION: nested for loop not needed
     // because every row in the matrix is exactly the same
-	for (i=0; i<height; i++) {
-		newRow.clear();
-		for (j=0; j<width; j++) {
-			newRow.push_back(0.0);
-		}
-		newGrid.push_back(newRow);
-	}
-	return newGrid;
+
+	vector < vector <float> > grid(height, vector<float>(width, 0.0));
+	return grid;
 }
