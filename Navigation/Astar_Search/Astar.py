424161771935dd215221f6cbd038ce63a898f636
Thanh Doan <doant@outlook.com>
2019-07-07 11:03:07 -0700

    Starting code for the google-maps style route planner.

---
+++ Navigation/Astar_Search/Astar.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from helpers import Map, load_map_10, load_map_40, show_map
+import math
+
+# %load_ext autoreload
+# %autoreload 2
+
+map_10 = load_map_10()
+show_map(map_10)
